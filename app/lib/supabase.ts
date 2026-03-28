ffa216040a51379c300a9897fdc911033693b6ec
albrt7 <alberto.cirimpei@student.upt.ro>
2026-03-28 21:24:23 +0200

    baza de date

--- app/lib/supabase.ts
+++ app/lib/supabase.ts
@@ -1,8 +1,10 @@
 import AsyncStorage from "@react-native-async-storage/async-storage";
 import { createClient } from "@supabase/supabase-js";
+import "react-native-url-polyfill/auto";
 
-const supabaseUrl = "URL_UL_TAU_DIN_SUPABASE";
-const supabaseAnonKey = "KEY_UL_TAU_ANON_DIN_SUPABASE";
+const supabaseUrl = "https://kgqgiprdojdqjjniyiwm.supabase.co";
+// Copiază cheia lungă din screenshot-ul tău în locul ghilimelelor de mai jos:
+const supabaseAnonKey = "ssb_publishable_z7dcC5t-ERJ25hACfJ6u4Q_nKbD5IkB";
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey, {
   auth: {
